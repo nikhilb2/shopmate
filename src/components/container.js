6ee8e01a4cf3f15eefcecf43d7ba9a878b168a0a
nikhil bhatia <nikhilbhatia2004@gmail.com>
2019-05-28 16:05:26 +0300

    banner text

--- src/components/container.js
+++ src/components/container.js
@@ -1,22 +1,47 @@
 import React from 'react';
 import { withStyles } from '@material-ui/styles';
+import Typography from '@material-ui/core/Typography';
 
 const styles = {
   root: {
 
   },
   imageContainer: {
+    width:'100%',
+    height:'100vh',
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "center center",
+    backgroundSize: "cover",
+  },
+  heading: {
+    position:'absolute',
+    color: 'white',
+    margin:'9rem',
+    marginTop: '5.5rem',
+    width: '50%',
+    lineHeight: '6rem',
+    wordSpacing: '6rem'
+  },
+  caption: {
+    position:'absolute',
+    color: 'white',
+    margin:'9rem',
+    marginTop: '19.5rem',
+    width: '50%',
+    fontSize: '2rem'
 
   }
 };
 
 const  SimpleContainer = (props) => {
-  const { image, text } = props
+  const { image, text, caption, classes } = props
   return (
     <div>
-      <div>
-        <img style={{width:'100%'}} src={image} alt={text} />
+      <div className={classes.imageContainer} style={{backgroundImage:`url(${image})`}}>
+        <Typography className={classes.heading} variant="h2">{text}</Typography>
+        <Typography className={classes.caption} variant="body1">{caption}</Typography>
       </div>
+
     </div>
   );
 }
--- src/components/homepage.js
+++ src/components/homepage.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import NavigationBar from './navBar'
 import SimpleContainer from './container'
+const text = "Background and development"
+const caption = "Convergent the dictates of the costumer: background and development"
 class Homepage extends Component {
   render() {
     return (
       <div>
         <NavigationBar />
-        <SimpleContainer image='static/banner.png' text="text" />
+        <SimpleContainer image='static/banner.png' text={text} caption={caption}/>
       </div>
     )
   }
