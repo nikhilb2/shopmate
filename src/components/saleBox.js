68734ffe0ab74ccca7c151233b08da925ed85879
nikhil bhatia <nikhilbhatia2004@gmail.com>
2019-05-28 20:42:04 +0300

    sale box dummy

--- src/components/saleBox.js
+++ src/components/saleBox.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { withStyles } from '@material-ui/styles';
 import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+import ButtonBase from '@material-ui/core/ButtonBase';
+import Typography from '@material-ui/core/Typography';
 
 const styles = {
   box: {
@@ -20,7 +23,36 @@ class SaleBox extends Component {
       p={1}
       className={classes.box}
     >
-      boxShadow={0}
+    <Grid container spacing={2}>
+       <Grid item>
+         <ButtonBase className={classes.image}>
+           <img className={classes.img} alt="complex" src="static/sumka.png" />
+         </ButtonBase>
+       </Grid>
+       <Grid item xs={12} sm container>
+         <Grid item xs container direction="column" spacing={2}>
+           <Grid item xs>
+             <Typography gutterBottom variant="subtitle1">
+               Standard license
+             </Typography>
+             <Typography variant="body2" gutterBottom>
+               Full resolution 1920x1080 • JPEG
+             </Typography>
+             <Typography variant="body2" color="textSecondary">
+               ID: 1030114
+             </Typography>
+           </Grid>
+           <Grid item>
+             <Typography variant="body2" style={{ cursor: 'pointer' }}>
+               Remove
+             </Typography>
+           </Grid>
+         </Grid>
+         <Grid item>
+           <Typography variant="subtitle1">$19.00</Typography>
+         </Grid>
+       </Grid>
+     </Grid>
     </Box>
     )
   }
