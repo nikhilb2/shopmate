d9f7e3dff985f9584174144c6ddaf83d4f2394ac
nikhil bhatia <nikhilbhatia2004@gmail.com>
2019-05-31 14:19:30 +0300

    homepage responsive

--- src/containers/cardBoxCont.js
+++ src/containers/cardBoxCont.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { withStyles } from '@material-ui/styles'
 import MiniBox from '../components/miniBox'
 import RegisterCard from '../components/registerCard'
+import Hidden from '@material-ui/core/Hidden'
 
 const styles = {
   container: {
@@ -19,24 +20,27 @@ const CBContainer = props => {
   const { classes } = props
   return (
     <div className={classes.container}>
-      <div className={classes.miniBox}>
-        <MiniBox
-          style={{ backgroundColor: '#84E6F1', marginTop: 0 }}
-          color="black"
-          colorCaption="#F62F5E"
-          title="WOW"
-          caption="Check WHAT!"
-        />
-        <MiniBox
-          style={{
-            backgroundImage: "url('static/men.png')",
-            backgroundSize: 'cover',
-            marginTop: 0
-          }}
-          title="MEN"
-          color="white"
-        />
-      </div>
+    <Hidden only={['xl', 'sm']} implementation="css">
+    <div className={classes.miniBox}>
+      <MiniBox
+        style={{ backgroundColor: '#84E6F1', marginTop: 0 }}
+        color="black"
+        colorCaption="#F62F5E"
+        title="WOW"
+        caption="Check WHAT!"
+      />
+      <MiniBox
+        style={{
+          backgroundImage: "url('static/men.png')",
+          backgroundSize: 'cover',
+          marginTop: 0
+        }}
+        title="MEN"
+        color="white"
+      />
+    </div>
+    </Hidden>
+
       <div>
         <RegisterCard />
       </div>
